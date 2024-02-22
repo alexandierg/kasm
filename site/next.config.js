c2f615495ead2d658cb8a03d8a2875d33b8c1490
alexandierg <125747682+alexandierg@users.noreply.github.com>
2024-02-22 11:50:26 -0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'Linphone',
     description: 'Softphone para Kasm.',
-    icon: '/img/logo.svg',
+    icon: 'https://styles.redditmedia.com/t5_pzxlx/styles/profileIcon_snooe518dd85-f77f-4725-ac4a-464846a19b97-headshot.pn',
     listUrl: 'https://alexandierg.github.io/kasm/',
     contactUrl: 'https://github.com/alexandierg/kasm/issues',
   },
